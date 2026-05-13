7fbba5a8e85aa8baeb815ed74de6ad5e48a741ed
jhsui <jiahansui1998@gmail.com>
2026-05-13 09:53:14 -0400

    done

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 import { createServer } from "node:http";
-import fs from "node:fs/promises";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import fs from "node:fs";
 
 const hostname = "localhost";
 const port = 8080;
 
 const requestListener = function (req, res) {
-  fs.readFile(join(__dirname + "/index.html"))
-    .then((contents) => {
-      res.setHeader("Content-Type", "text/html");
-      res.writeHead(200);
-      res.end(contents);
-    })
-    .catch((err) => {
-      res.writeHead(500);
-      res.end("Internal Server Error");
+  let fileName = "";
+
+  switch (req.url) {
+    case "/":
+      fileName = "index.html";
+      break;
+
+    case "/contact-me":
+      fileName = "contact-me.html";
+      break;
+
+    case "/about":
+      fileName = "about.html";
+      break;
+
+    default:
+      fileName = "404.html";
+  }
+
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "content-type": "text/plain" });
+      res.end("server error");
       return;
-    });
+    } else {
+      res.writeHead(fileName === "404.html" ? 404 : 200, {
+        "content-type": "text/html",
+      });
+      res.end(data);
+    }
+  });
 };
 
 const server = createServer(requestListener);
