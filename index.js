a7e787353c7a0842bd14fc4a6c1f386ec648b5e1
jhsui <jiahansui1998@gmail.com>
2026-05-14 14:04:57 -0400

    use express js

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
-import { createServer } from "node:http";
-import fs from "node:fs/promises";
 import { join } from "node:path";
-
-const __dirname = import.meta.dirname;
+import express from "express";
 
 const hostname = "localhost";
 const port = 8080;
 
-let indexFile;
-let aboutFile;
-let contactMeFile;
-let file404;
+const __dirname = import.meta.dirname;
 
-const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "text/html");
+const app = express();
 
-  switch (req.url) {
-    case "/":
-      res.writeHead(200);
-      return res.end(indexFile);
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
 
-    case "/contact-me":
-      res.writeHead(200);
-      return res.end(contactMeFile);
+app.get("/about", (req, res) => {
+  res.sendFile(join(__dirname, "about.html"));
+});
 
-    case "/about":
-      res.writeHead(200);
-      return res.end(aboutFile);
+app.get("/contact-me", (req, res) => {
+  res.sendFile(join(__dirname, "contact-me.html"));
+});
 
-    default:
-      res.writeHead(404);
-      return res.end(file404);
+app.use((req, res) => {
+  res.status(404).sendFile(join(__dirname, "404.html"));
+});
+
+app.listen(port, hostname, (error) => {
+  if (error) {
+    throw error;
   }
-};
-
-const server = createServer(requestListener);
-
-Promise.all([
-  fs.readFile(join(__dirname, "index.html")),
-  fs.readFile(join(__dirname, "contact-me.html")),
-  fs.readFile(join(__dirname, "about.html")),
-  fs.readFile(join(__dirname, "404.html")),
-])
-  .then(([index, contact, about, notFound]) => {
-    indexFile = index;
-    contactMeFile = contact;
-    aboutFile = about;
-    file404 = notFound;
-
-    server.listen(port, hostname, () => {
-      console.log(`Server running at http://${hostname}:${port}/`);
-    });
-  })
-  .catch((err) => {
-    console.error(`Could not read files: ${err}`);
-    process.exit(1);
-  });
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
